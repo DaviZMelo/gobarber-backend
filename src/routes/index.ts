1befd095daac705e9863273f153cdd3da1d54f39
Davi Melo <extremexgamer32@gmail.com>
2020-12-22 04:54:08 -0300

    feat: add errors handling

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,10 +6,6 @@ import sessionRouter from './session.routes';
 
 const routes = Router();
 
-routes.use(sessionRouter);
-routes.use(appointmentsRouter);
-routes.use(usersRouter);
-
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
