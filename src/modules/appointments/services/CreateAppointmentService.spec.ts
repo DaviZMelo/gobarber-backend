50f3a548fe5ea6a5ba7b98405f87e13159b0a6c9
Davi Melo <extremexgamer32@gmail.com>
2020-12-28 22:58:44 -0300

    test: add CreateAppointmentService test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointments', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1235166125',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1235166125');
+  });
+
+  it('should be not able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '1235166125',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '1235166125',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
